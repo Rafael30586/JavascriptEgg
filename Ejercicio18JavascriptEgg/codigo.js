ba7e22c45e6e97e67b5cd0a5b2780b16767ec352
F. Rafael Alvarez <110036242+Rafael30586@users.noreply.github.com>
2023-08-16 19:20:13 -0300

    Ejercicio 18

--- Ejercicio18JavascriptEgg/codigo.js
+++ Ejercicio18JavascriptEgg/codigo.js
@@ -1,4 +1,25 @@
 
 var valores = [true,5,false,"hola","adios",2];
 
-//no logro entende del todo las consignas, lo dejo para después
+if(valores[3].length>valores[4].length){
+	alert("La cadena de la posicion 3 tiene mayor longitud");
+}else if(valores[3].length<valores[4].length){
+	alert("La cadena de la posicion 4 tiene mayor longitud");
+}else{
+	alert("Ambas cadenas tienen la misma longitud");
+}
+
+valores.push(valores[0]||valores[2]);
+valores.push(valores[0]&&valores[2]);
+
+const suma = valores[1] + valores[5];
+const resta = valores[1] - valores[5];
+const producto = valores[1] * valores[5];
+const division = valores[1] / valores[5];
+const modulo = valores[1] % valores[5];
+
+alert(`Suma: ${suma}
+	Resta: ${resta}
+	Producto: ${producto}
+	Division: ${division}
+	Modulo: ${modulo}`);
