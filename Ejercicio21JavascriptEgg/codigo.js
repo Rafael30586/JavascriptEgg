02f6ee1184d1208e86b0916abf1067c5c6c111d1
F. Rafael Alvarez <110036242+Rafael30586@users.noreply.github.com>
2023-08-16 19:42:42 -0300

    Ejercicio 21

---
+++ Ejercicio21JavascriptEgg/codigo.js
@@ -0,0 +1,13 @@
+
+var Persona = {Nombre:"Rafael",Edad:5,Sexo:"Hombre",Peso:200,Altura:1.14};
+//console.log(Persona);
+
+var arr = [];
+
+arr.push(Persona.Nombre);
+arr.push(Persona.Edad);
+arr.push(Persona.Sexo);
+arr.push(Persona.Peso);
+arr.push(Persona.Altura);
+
+console.log(arr);
